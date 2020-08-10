60ceb8ead0507567bee82040e6d5c54226959bd0
adalbertobrant <adalbertobrant@gmail.com>
2020-08-10 06:31:04 -0300

    refatoramento do código consulores, page

--- src/pages.js
+++ src/pages.js
@@ -1,4 +1,6 @@
 const { subjects, weekdays, getSubject } = require('./utils/format');
+const Database = require('./database/db');
+
 //pagina inicial
 function pageLanding(req, res) {
   return res.render('index.html');
@@ -6,8 +8,26 @@ function pageLanding(req, res) {
 //pagina consultores - professores
 function consultores(req, res) {
   const filters = req.query;
+  if (!filters.subject || !filters.weekday || !filters.time) {
+    return res.render('consultores.html', { filters, subjects, weekdays });
+  }
+
+  const query = `
+  	SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE EXISTS (
+		SELECT class_schedule.* 
+    	FROM class_schedule
+    	WHERE class_schedule.class_id = classes.id
+    	AND class_schedule.weekday = ${filters.weekday}
+    	AND class_schedule.time_from <= ${filters.time_from}
+    	AND class_schedule.time_to > ${filters.time_to}
+	)
+  `;
+
   return res.render('consultores.html', {
-    consults,
+    //   consults,
     filters,
     subjects,
     weekdays,
