7a42720325ea17bb55257a7c2378179ef2e5ffa4
adalbertobrant <adalbertobrant@gmail.com>
2020-08-10 05:36:53 -0300

    criação do fake_data.js para testes de usuários

--- src/server.js
+++ src/server.js
@@ -2,35 +2,6 @@ const express = require('express');
 const server = express();
 const nunjuncks = require('nunjucks');
 
-// dados dinâmicos
-const consults = [
-  {
-    name: 'Adalberto Caldeira',
-    avatar:
-      'https://avatars3.githubusercontent.com/u/6125981?s=460&u=c1f198b08c3f5e7b893be3eac65873d274d918e3&v=4',
-    whatsapp: '34991179515',
-    bio:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.',
-    subject: 'Oclusão',
-    cost: '50',
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    time_from: [720],
-    time_to: [2000],
-  },
-  {
-    name: 'Adalberto C. Brant Filho',
-    avatar:
-      'https://avatars3.githubusercontent.com/u/6125981?s=460&u=c1f198b08c3f5e7b893be3eac65873d274d918e3&v=4',
-    whatsapp: '34991179515',
-    bio:
-      'Cirurgião Dentista, especialista em Ortodontia.Tem mestrado em clínica odontológica com área de concentração em prótese sobre implantes.Desenvolvimento de e-books digitais.',
-    subject: 'Cefalometria',
-    cost: '100',
-    weekday: [3],
-    time_from: [720],
-    time_to: [1720],
-  },
-];
 // consultorias disponíveis
 const subjects = [
   'Planejamento Ortodôntico',
@@ -57,6 +28,7 @@ const weekdays = [
   'Sexta-feira',
   'Sábado',
 ];
+
 //transforma número em string
 function getSubject(subjectNumber) {
   const position = +subjectNumber - 1;
