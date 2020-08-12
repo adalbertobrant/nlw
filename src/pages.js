3c0f42631059d127405cd85a8ed18ea6c0077cda
adalbertobrant <adalbertobrant@gmail.com>
2020-08-12 05:13:34 -0300

    Finalização do projeto nlw proffy com modificações

--- src/pages.js
+++ src/pages.js
@@ -6,15 +6,13 @@ function pageLanding(req, res) {
   return res.render('index.html');
 }
 //pagina consultores - professores
-function consultores(req, res) {
+async function consultores(req, res) {
   const filters = req.query;
   if (!filters.subject || !filters.weekday || !filters.time) {
     return res.render('consultores.html', { filters, subjects, weekdays });
   }
-
   // conversão de horas para minutos
   const epochMinutes = toMinutes(filters.time);
-
   const query = `
   	SELECT classes.*, proffys.*
     FROM proffys
@@ -26,31 +24,73 @@ function consultores(req, res) {
     	AND class_schedule.weekday = ${filters.weekday}
     	AND class_schedule.time_from <= ${epochMinutes}
     	AND class_schedule.time_to > ${epochMinutes}
-	)
+  )
+  AND classes.subject = '${filters.subject}'
   `;
-
-  return res.render('consultores.html', {
-    //   consults,
+  // em caso de erro na hora da consulta do banco de dados
+  try {
+    const db = await Database;
+    const consults = await db.all(query);
+    consults.map((consult) => {
+      consult.subject = getSubject(consult.subject);
+    });
+    return res.render('consultores.html', {
+      consults,
+      subjects,
+      filters,
+      weekdays,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  /*return res.render('consultores.html', {
+    //consults,
     filters,
     subjects,
     weekdays,
-  });
+  });*/
 }
 // Página de formulário para professores
 function giveClasses(req, res) {
-  const data = req.query;
-  const isEmpty = Object.keys(data).length > 0;
-  // adicionar a variável data a lista de consults
-  if (isEmpty) {
-    data.subject = getSubject(data.subject);
-    consults.push(data);
-    return res.redirect('/consultores');
-  } else {
-    return res.render('give-classes.html', { subjects, weekdays });
+  return res.render('give-classes.html', { subjects, weekdays });
+}
+// método post para receber os dados do formulário
+
+async function saveClasses(req, res) {
+  const createProffy = require('./database/createProffy');
+  const proffyValue = {
+    name: req.body.name,
+    avatar: req.body.avatar,
+    whatsapp: req.body.whatsapp,
+    bio: req.body.bio,
+  };
+  const classValue = {
+    subject: req.body.subject,
+    cost: req.body.cost,
+  };
+  const classScheduleValues = req.body.weekday.map((weekday, index) => {
+    return {
+      weekday,
+      time_from: toMinutes(req.body.time_from[index]),
+      time_to: toMinutes(req.body.time_to[index]),
+    };
+  });
+  try {
+    const db = await Database;
+    await createProffy(db, { proffyValue, classValue, classScheduleValues });
+    let queryString = '?subject=' + getSubject(req.body.subject);
+    console.log(getSubject(req.body.subject));
+    queryString += '&weekday=' + req.body.weekday[0];
+    queryString += '&time=' + req.body.time_from[0];
+    return res.redirect('/consultores' + queryString);
+  } catch (error) {
+    console.log(error);
   }
 }
+
 module.exports = {
   pageLanding,
   consultores,
   giveClasses,
+  saveClasses,
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const server = express();
 const nunjuncks = require('nunjucks');
-const { pageLanding, consultores, giveClasses } = require('./pages');
+const {
+  pageLanding,
+  consultores,
+  giveClasses,
+  saveClasses,
+} = require('./pages');
 
 //configuração do nunjuncks
 nunjuncks.configure('src/views', {
@@ -9,10 +14,13 @@ nunjuncks.configure('src/views', {
   noCache: true,
 });
 server
+  //receber os dados do req.body
+  .use(express.urlencoded({ extended: true }))
   //arquivos estáticos (imagens, css,scripts)
   .use(express.static('public'))
   //rotas da aplicação
   .get('/', pageLanding)
   .get('/consultores', consultores)
   .get('/give-classes', giveClasses)
+  .post('/save-classes', saveClasses)
   .listen(5500);
